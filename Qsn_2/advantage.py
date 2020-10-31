56d91b3011bb9ca570d89a934b162a5f26aa6140
Sudhamshu B N <66492363+sudhamshu091@users.noreply.github.com>
2020-10-31 21:04:03 +0530

    Add files via upload

---
+++ Qsn_2/advantage.py
@@ -0,0 +1,17 @@
+#not my solution
+def advantage(A, B):
+        sortedA = sorted(A)
+        sortedB = sorted(B)
+
+        assigned = {b: [] for b in B}
+        remaining = []
+
+        j = 0
+        for a in sortedA:
+            if a > sortedB[j]:
+                assigned[sortedB[j]].append(a)
+                j += 1
+            else:
+                remaining.append(a)
+        return [assigned[b].pop() if assigned[b] else remaining.pop() for b in B]
+print(advantage([2,7,11,15],[1,10,4,11]))
