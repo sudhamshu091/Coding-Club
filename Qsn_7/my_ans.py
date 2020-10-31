e91fdfa1807397f22a6fe5ac75045c279b9139cb
Sudhamshu B N <66492363+sudhamshu091@users.noreply.github.com>
2020-10-31 14:57:27 +0530

    Update my_ans.py

--- Qsn_7/my_ans.py
+++ Qsn_7/my_ans.py
@@ -1,1 +1,11 @@
-
+def maxweight(people, limit):
+    people.sort()
+    i, j = 0, len(people) - 1
+    ans = 0
+    while i <= j:
+        ans += 1
+        if people[i] + people[j] <= limit:
+            i += 1
+        j -= 1
+    return ans
+print(maxweight([32,3,45,5,5,74,34,56],73))
