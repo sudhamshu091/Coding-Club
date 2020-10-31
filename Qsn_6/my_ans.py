1f48787914fa1b987ddfb097e1a628ec727af014
Sudhamshu B N <66492363+sudhamshu091@users.noreply.github.com>
2020-10-31 14:20:27 +0530

    Update my_ans.py

--- Qsn_6/my_ans.py
+++ Qsn_6/my_ans.py
@@ -1,7 +1,10 @@
-x = [[1,2],[3,4]]
+x = [[1,2],[3,7],[1,1]]
 print(x)
 a =0
+list = []
 for i in range(len(x)):
         for j in range(len(x[0])):
-            a = a + x[i][j] + 1
+            if x[i][j] not in list:
+                list.append(x[i][j])
+                a = a + x[i][j]+1
 print(a+3)
